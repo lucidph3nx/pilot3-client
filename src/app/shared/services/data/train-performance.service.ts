4f3ed3f49847b657bba39cafa7c5fbb92ed2b24c
Ben Sherman <Ben.Sherman@transdev.com.au>
2020-01-06 10:50:43 +1300

    added MTD and WTD options for Train Performance

--- src/app/shared/services/data/train-performance.service.ts
+++ src/app/shared/services/data/train-performance.service.ts
@@ -28,10 +28,10 @@ export class TrainPerformanceService {
     //     .map((response: Response) => response.json))
     // }
 
-    getTrainPerformance = () => {
+    getTrainPerformance = (dateFrom, dateTo) => {
         return Observable
         .timer(0,10000)
-        .switchMap(() => this.http.get('http://'+environment.apiURL+':4000/api/performance/day')
+        .switchMap(() => this.http.get('http://'+environment.apiURL+':4000/api/performance/day?dateFrom='+dateFrom.format("YYYYMMDD") +'&dateTo='+dateTo.format("YYYYMMDD"))
         .map((response: Response) => new trainPerformanceResponse().fromJSON(response)))
     }
 
